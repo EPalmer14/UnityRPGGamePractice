98347315e3c2b89cc171200aa942262b99251b9a
EPalmer14 <zekepalmer14@gmail.com>
2025-01-09 00:09:47 -0500

    finishing up attack

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -44,7 +44,7 @@ public class Player : MonoBehaviour
     public PlayerWallSlideState wallSlide { get; private set; }
     public PlayerWallJumpState wallJump { get; private set; }
 
-    public PlayerPrimaryAttack primaryAttack { get; private set; }
+    public PlayerPrimaryAttackState primaryAttack { get; private set; }
     #endregion
 
     private void Awake()
@@ -59,7 +59,7 @@ public class Player : MonoBehaviour
         wallSlide = new PlayerWallSlideState(this, stateMachine, "WallSlide");
         wallJump = new PlayerWallJumpState(this, stateMachine, "Jump");
 
-        primaryAttack = new PlayerPrimaryAttack(this, stateMachine, "Attack");
+        primaryAttack = new PlayerPrimaryAttackState(this, stateMachine, "Attack");
     }
 
     private void Start()
--- Assets/PlayerPrimaryAttack.cs
+++ Assets/PlayerPrimaryAttack.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
-public class PlayerPrimaryAttack : PlayerState
+public class PlayerPrimaryAttackState : PlayerState
 {
     private int comboCounter;
     private float lastTimeAttacked;
     private float comboWindow = 1;
-    public PlayerPrimaryAttack(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    public PlayerPrimaryAttackState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
 
     }
@@ -25,7 +25,14 @@ public class PlayerPrimaryAttack : PlayerState
         player.anim.SetInteger("ComboCounter", comboCounter);
         //player.anim.speed = 1.5f;
 
-        player.SetVelocity(player.attackMovement[comboCounter].x * player.facingDir, player.attackMovement[comboCounter].y);
+
+        float attackDir = player.facingDir;
+        if (xInput != 0)
+        {
+            attackDir = xInput;
+        }
+
+        player.SetVelocity(player.attackMovement[comboCounter].x * attackDir, player.attackMovement[comboCounter].y);
 
         stateTimer = .1f;
     }
