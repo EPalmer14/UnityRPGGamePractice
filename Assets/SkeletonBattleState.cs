ed33bd197d5f481454fe94440a1456bea6124b78
EPalmer14 <zekepalmer14@gmail.com>
2025-01-23 22:02:33 -0500

    enemy aggression timer

--- Assets/SkeletonBattleState.cs
+++ Assets/SkeletonBattleState.cs
@@ -26,12 +26,21 @@ public class SkeletonBattleState : EnemyState
 
         if (enemy.IsPlayerDetected())
         {
+            stateTimer = enemy.battleTime;
+
             if (enemy.IsPlayerDetected().distance < enemy.attackDistance)
             {
                 if (CanAttack())
                 {
                     stateMachine.ChangeState(enemy.attackState);
                 }
+            }
+        }
+        else
+        {
+            if (stateTimer < 0)
+            {
+                stateMachine.ChangeState(enemy.idleState);
             }
         }
 
