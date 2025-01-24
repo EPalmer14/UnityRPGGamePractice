23a2a1cc300eaa1be245cc2e87c26ff8f6c48c82
EPalmer14 <zekepalmer14@gmail.com>
2025-01-23 21:06:35 -0500

    Skeleton attack state

---
+++ Assets/SkeletonAttackState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonAttackState : EnemyState
+{
+    public SkeletonAttackState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName, Enemy_Skeleton enemy) : base(_enemyBase, _stateMachine, _animBoolName)
+    {
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
