14ef4e53940def8834b566ac48318c8542a10f98
EPalmer14 <zekepalmer14@gmail.com>
2025-01-29 19:47:09 -0500

    Knockback

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -10,6 +10,11 @@ public class Entity : MonoBehaviour
     public EntityFX fx { get; private set; }
     #endregion
 
+    [Header("Knockback Info")]
+    [SerializeField] protected Vector2 knockbackDirection;
+    [SerializeField] protected float knockbackDuration;
+    protected bool isKnocked;
+
     [Header("Collision Info")]
     public Transform attackCheck;
     public float attackCheckRadius;
@@ -44,13 +49,29 @@ public class Entity : MonoBehaviour
     public virtual void Damage()
     {
         fx.StartCoroutine("FlashFX");
+        StartCoroutine("HitKnockback");
         Debug.Log(gameObject.name + ": I was damaged!");
+    }
+
+    protected virtual IEnumerator HitKnockback()
+    {
+        isKnocked = true;
+
+        rb.velocity = new Vector2(knockbackDirection.x * -facingDir, knockbackDirection.y);
+
+        yield return new WaitForSeconds(knockbackDuration);
+        isKnocked = false;
     }
 
     #region Velocity
     public void SetZeroVelocity() => rb.velocity = new Vector2(0, 0);
     public void SetVelocity(float _xVelocity, float _yVelocity)
     {
+        if (isKnocked)
+        {
+            return;
+        }
+
         rb.velocity = new Vector2(_xVelocity, _yVelocity);
         FlipController(_xVelocity);
     }
