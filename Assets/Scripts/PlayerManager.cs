4b869bb44ee6e310b6c7cbbce0cfcc495a8a5e33
EPalmer14 <zekepalmer14@gmail.com>
2025-02-05 19:49:20 -0500

    replace game object finds with PlayerManager instance

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static PlayerManager instance;
+    public Player player;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        instance = this;
     }
 }
