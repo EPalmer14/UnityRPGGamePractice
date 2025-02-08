12cc62f801de06a154e704b41cdead8b3bc13dd6
EPalmer14 <zekepalmer14@gmail.com>
2025-02-07 20:59:53 -0500

    Dash skill script

--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -11,4 +11,22 @@ public class Skill : MonoBehaviour
     {
         cooldownTimer -= Time.deltaTime;
     }
+
+    public virtual bool CanUseSkill()
+    {
+        if (cooldownTimer < 0)
+        {
+            UseSkill();
+            cooldownTimer = cooldown;
+            return true;
+        }
+
+        Debug.Log("Skill is on cooldown");
+        return false;
+    }
+
+    public virtual void UseSkill()
+    {
+
+    }
 }
