8df8ce0bf7cf13a44def9addd945591b599d61bc
EPalmer14 <zekepalmer14@gmail.com>
2025-02-11 22:17:06 -0500

    Setup clone attack in controller

--- Assets/CloneSkillController.cs
+++ Assets/CloneSkillController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class CloneSkillController : MonoBehaviour
 {
     private SpriteRenderer sr;
+    private Animator anim;
     [SerializeField] private float colorLosingSpeed;
     private float cloneTimer;
 
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -23,8 +25,12 @@ public class CloneSkillController : MonoBehaviour
         }
     }
 
-    public void SetupClone(Transform _newTransform, float _cloneDuration)
+    public void SetupClone(Transform _newTransform, float _cloneDuration, bool _canAttack)
     {
+        if (_canAttack)
+        {
+            anim.SetInteger("AttackNumber", Random.Range(1, 3));
+        }
         transform.position = _newTransform.position;
         cloneTimer = _cloneDuration;
     }
--- Assets/Scripts/Skills/CloneSkill.cs
+++ Assets/Scripts/Skills/CloneSkill.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class CloneSkill : MonoBehaviour
 {
+
+
     [Header("Clone Info")]
     [SerializeField] private GameObject clonePrefab;
     [SerializeField] private float cloneDuration;
+    [Space]
+    [SerializeField] private bool canAttack;
 
     public void CreateClone(Transform _clonePosition)
     {
         GameObject newClone = Instantiate(clonePrefab);
 
-        newClone.GetComponent<CloneSkillController>().SetupClone(_clonePosition, cloneDuration);
+        newClone.GetComponent<CloneSkillController>().SetupClone(_clonePosition, cloneDuration, canAttack);
     }
 }
