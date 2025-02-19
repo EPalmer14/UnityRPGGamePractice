ec88a711b815bb7f6a0104131b1398fcef7fb51c
EPalmer14 <zekepalmer14@gmail.com>
2025-02-18 23:05:43 -0500

    Set Sword skill controller

--- Assets/SwordSkillController.cs
+++ Assets/SwordSkillController.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class SwordSkillController : MonoBehaviour
 {
+    private Animator anim;
+    private Rigidbody2D rb;
+    private CircleCollider2D cd;
+    private Player player;
 
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        cd = GetComponent<CircleCollider2D>();
+    }
 }
