7f68d8e887279bf211ea58d952145a5b681869f0
EPalmer14 <zekepalmer14@gmail.com>
2025-02-19 22:02:25 -0500

    setup aim ui

--- Assets/Scripts/Skills/SwordSkill.cs
+++ Assets/Scripts/Skills/SwordSkill.cs
@@ -7,14 +7,33 @@ public class SwordSkill : Skill
 {
     [Header("Skill Info")]
     [SerializeField] private GameObject swordPrefab;
-    [SerializeField] private Vector2 launchDir;
+    [SerializeField] private Vector2 launchForce;
     [SerializeField] private float swordGravity;
+
+    private Vector2 finalDir;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            finalDir = new Vector2(AimDirection().normalized.x * launchForce.x, AimDirection().normalized.y * launchForce.y);
+        }
+    }
 
     public void CreateSword()
     {
         GameObject newSword = Instantiate(swordPrefab, player.transform.position, transform.rotation);
         SwordSkillController newSwordScript = newSword.GetComponent<SwordSkillController>();
 
-        newSwordScript.SetupSword(launchDir, swordGravity);
+        newSwordScript.SetupSword(launchForce, swordGravity);
+    }
+
+    public Vector2 AimDirection()
+    {
+        Vector2 playerPosition = player.transform.position;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - playerPosition;
+
+        return direction;
     }
 }
