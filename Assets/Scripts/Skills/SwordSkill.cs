cd6da2d32943c578c3a74099519625fbfa822993
EPalmer14 <zekepalmer14@gmail.com>
2025-02-19 21:33:39 -0500

    Give sword skill access to player

--- Assets/Scripts/Skills/SwordSkill.cs
+++ Assets/Scripts/Skills/SwordSkill.cs
@@ -12,6 +12,6 @@ public class SwordSkill : Skill
 
     public void CreateSword()
     {
-        GameObject newSword = Instantiate(swordPrefab);
+        GameObject newSword = Instantiate(swordPrefab, );
     }
 }
